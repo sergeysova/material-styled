bc9e8197778f2c6f256918e851cf9cf78ed75d8d
Sergey Sova <i.am@lestad.net>
2017-08-15 03:14:44 +0300

    Add version selector to component generator

--- scripts/create.component.js
+++ scripts/create.component.js
@@ -6,6 +6,7 @@ const Inquirer = require('inquirer')
 const ProgressBar = require('progress')
 const chalk = require('chalk')
 const changeCase = require('change-case')
+const semver = require('semver')
 const glob = pify(require('glob'))
 const mkdirp = pify(require('mkdirp'))
 
@@ -16,6 +17,8 @@ const mkdirp = pify(require('mkdirp'))
 const REJECTED_PACKAGES = ['core']
 const AVAILABLE_BUNDLES = [{ name: 'core', checked: true }]
 const BASE_PACKAGE = 'Paper'
+const INITIAL_VERSION = '0.0.1'
+const LIBRARY_NAME = require('../package.json').name
 
 
 async function readPackages() {
@@ -60,6 +63,18 @@ async function createQuestions() {
     return true
   }
 
+  const versionValidator = (value) => {
+    if (!semver.valid(value)) {
+      return 'Please, enter valid semver version'
+    }
+
+    if (semver.major(value) !== 0) {
+      return 'New component should be 0.Y.Z version'
+    }
+
+    return true
+  }
+
   return [
     {
       name: 'componentName',
@@ -69,6 +84,13 @@ async function createQuestions() {
       validate: componentNameValidator,
     },
     {
+      name: 'version',
+      type: 'input',
+      message: 'Initial version of a new component:',
+      default: INITIAL_VERSION,
+      validate: versionValidator,
+    },
+    {
       name: 'baseComponentName',
       type: 'list',
       message: 'Select base component name:',
@@ -76,7 +98,7 @@ async function createQuestions() {
       choices: listOfPackages,
     },
     {
-      name: 'appendTo',
+      name: 'appendToScopes',
       type: 'checkbox',
       message: 'Add package to:',
       choices: AVAILABLE_BUNDLES,
@@ -112,7 +134,7 @@ async function processComponentFiles(baseName, targetName, list) {
       .replace(pkg.description, `{Description for ${targetName}}`)
       .replace(new RegExp(baseNamePath, 'g'), targetNamePath)
       .replace(new RegExp(baseName, 'g'), targetName)
-      .replace(new RegExp(pkg.version, 'g'), '0.0.1')
+      .replace(new RegExp(pkg.version, 'g'), INITIAL_VERSION)
 
     await mkdirp(dirname(targetPath))
     await writeFile(targetPath, newSource, { encoding: 'utf8' })
@@ -146,12 +168,48 @@ async function askForCreation(baseName, targetName, list) {
 }
 
 
+async function addToScopes(targetName, scopes) {
+  const pathName = changeCase.paramCase(targetName)
+
+  if (scopes.length) {
+    for (const scope of scopes) {
+      try {
+        const packagePath = resolve(__dirname, '..', 'packages', scope, 'package.json')
+        const readmePath = resolve(__dirname, '..', 'packages', scope, 'readme.md')
+        const indexPath = resolve(__dirname, '..', 'packages', scope, 'src', 'index.js')
+
+        const pkg = JSON.parse(await readFile(packagePath, 'utf8'))
+        const readme = await readFile(readmePath, 'utf8')
+        let index = await readFile(indexPath, 'utf8')
+
+        pkg.dependencies[`@${LIBRARY_NAME}/${pathName}`] = `^${INITIAL_VERSION}`
+        index += `- ${targetName}\n`
+        index += `export ${targetName} from '@${LIBRARY_NAME}/${pathName}'\n`
+
+        await writeFile(packagePath, JSON.stringify(pkg, 2, 2), { encoding: 'utf8' })
+        await writeFile(readmePath, readme, { encoding: 'utf8' })
+        await writeFile(indexPath, index, { encoding: 'utf8' })
+
+        console.log(`Component ${chalk.bold.white(targetName)} added to scope ${chalk.bold(scope)}`)
+      }
+      catch (error) {
+        debug(`add ${targetName} to scope ${scope}`, error, error.stack)
+        console.log('!', chalk.yellow(`${targetName} not added to scope ${scope}`))
+      }
+    }
+  }
+}
+
+
 async function main() {
   const options = await Inquirer.prompt(await createQuestions())
   const foundList = await findComponentFiles(options.baseComponentName)
 
   if (await askForCreation(options.baseComponentName, options.componentName, foundList)) {
-    await processComponentFiles(options.baseComponentName, options.componentName, foundList)
+    // await processComponentFiles(options.baseComponentName, options.componentName, foundList)
+    await addToScopes(options.componentName, options.appendToScopes)
+    console.log('\n', chalk.white.bold('Complete!'), '\n')
+    console.log('Now run', chalk.green('npm run bs'), 'to install dependencies\n')
   }
   else {
     console.log(chalk.gray('Cancelled'))
