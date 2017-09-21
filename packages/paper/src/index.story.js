bb9915c62d925f7d45adfeb55a67172c894573ae
Sergey Sova <i.am@lestad.net>
2017-09-21 22:41:39 +0300

    docs(@paper): Fix type of `z` prop in story
    Prop `z` should be string, instead of number
    
    BREAKING CHANGE: Changed type of `z` prop in @paper

--- packages/paper/src/index.story.js
+++ packages/paper/src/index.story.js
@@ -12,26 +12,26 @@ storiesOf('core/Paper', module)
   ))
   .add('With z=3', () => (
     <div style={{ padding: '30px' }}>
-      <Paper z={3}>Z level 3</Paper>
+      <Paper z="3">Z level 3</Paper>
     </div>
   ))
   .add('With z=6', () => (
     <div style={{ padding: '30px' }}>
-      <Paper z={6}>Z level 6</Paper>
+      <Paper z="6">Z level 6</Paper>
     </div>
   ))
   .add('With z=12', () => (
     <div style={{ padding: '30px' }}>
-      <Paper z={12}>Z level 12</Paper>
+      <Paper z="12">Z level 12</Paper>
     </div>
   ))
   .add('With z=16', () => (
     <div style={{ padding: '30px' }}>
-      <Paper z={16}>Z level 16</Paper>
+      <Paper z="16">Z level 16</Paper>
     </div>
   ))
   .add('With z=24', () => (
     <div style={{ padding: '30px' }}>
-      <Paper z={24}>Z level 24</Paper>
+      <Paper z="24">Z level 24</Paper>
     </div>
   ))
