1c5da8345e0dc15569d9b0ce0cb95708d5951cfe
Sergey Sova <i.am@lestad.net>
2017-09-21 22:38:28 +0300

    chore(dependencies): Update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react'
 
 function loadStories() {
   const req = require.context('../packages/', true, /src\/\w+\.story\.js$/)
-  console.log({ KEYS: req.keys() })
+
   req.keys().forEach(req)
 }
 
