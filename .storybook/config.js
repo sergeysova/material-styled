1c7d61a099fc5a55d55daff36a9ed9d1aed2dac1
Sergey Sova <i.am@lestad.net>
2017-09-22 04:14:51 +0300

    docs(storybook): Add decorator for stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,19 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import styled from 'styled-components'
+import { configure, addDecorator } from '@storybook/react'
 
 function loadStories() {
   const req = require.context('../packages/', true, /src\/\w+\.story\.js$/)
 
   req.keys().forEach(req)
 }
+
+const RootDecorator = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 30px;
+`
+
+addDecorator(fn => <RootDecorator>{fn()}</RootDecorator>)
 
 configure(loadStories, module)
