3ab1fd919a147b9ec80054c4b0e88831cfed8de5
Sergey Sova <i.am@lestad.net>
2017-09-22 04:17:21 +0300

    docs(@paper): Remove custom decorators

--- packages/paper/src/index.story.js
+++ packages/paper/src/index.story.js
@@ -6,32 +6,32 @@ import Paper from './'
 
 storiesOf('core/Paper', module)
   .add('Without props', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper>Default z=1</Paper>
     </div>
   ))
   .add('With z=3', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper z="3">Z level 3</Paper>
     </div>
   ))
   .add('With z=6', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper z="6">Z level 6</Paper>
     </div>
   ))
   .add('With z=12', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper z="12">Z level 12</Paper>
     </div>
   ))
   .add('With z=16', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper z="16">Z level 16</Paper>
     </div>
   ))
   .add('With z=24', () => (
-    <div style={{ padding: '30px' }}>
+    <div>
       <Paper z="24">Z level 24</Paper>
     </div>
   ))
